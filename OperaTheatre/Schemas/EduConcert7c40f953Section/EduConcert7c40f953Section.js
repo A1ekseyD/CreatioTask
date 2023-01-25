fda891c3e8279777f4fd2e58c32831dff8d41c4f
Aleks <aalleksei@mail.ru>
2023-01-26 00:29:34 +0300

    фывф

---
+++ OperaTheatre/Schemas/EduConcert7c40f953Section/EduConcert7c40f953Section.js
@@ -0,0 +1,8 @@
+define("EduConcert7c40f953Section", [], function() {
+	return {
+		entitySchemaName: "EduConcert",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
